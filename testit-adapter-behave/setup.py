e2a67bc3146965556f0f70dca3d664fa4acd7d5a
Dmitry.Gridnev <dmitry.gridnev@testit.software>
2022-11-03 09:27:30 +0100

    Bump version

--- testit-adapter-behave/setup.py
+++ testit-adapter-behave/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='testit-adapter-behave',
-    version='2.0.1',
+    version='2.0.2',
     description='Behave adapter for Test IT',
     long_description=open('README.md', "r").read(),
     long_description_content_type="text/markdown",
