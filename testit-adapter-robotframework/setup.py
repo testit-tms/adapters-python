953612a1df77b51c5f588e4b37857e7afe225115
Pavel Butuzov <pavel.butuzov@testit.software>
2022-12-16 19:31:07 +0300

    Fixed updating of current versions of package dependencies

--- testit-adapter-robotframework/setup.py
+++ testit-adapter-robotframework/setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 
+package_version = '2.0.9'
+
 setup(
     name='testit-adapter-robotframework',
-    version='2.0.8',
+    version=package_version,
     description='Robot Framework adapter for Test IT',
     long_description=open('README.md', "r").read(),
     long_description_content_type="text/markdown",
@@ -20,5 +22,5 @@ setup(
     py_modules=['testit_adapter_robotframework'],
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
-    install_requires=['attrs', 'robotframework', 'testit-python-commons>=2,<3']
+    install_requires=['attrs', 'robotframework', f'testit-python-commons=={package_version}']
 )
