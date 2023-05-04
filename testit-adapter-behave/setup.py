d67d7a7a640812c4b9b703f0ea256bfed57fa0ce
Pavel Butuzov <pavel.butuzov@testit.software>
2023-05-04 15:09:11 +0300

    Updated versions.

--- testit-adapter-behave/setup.py
+++ testit-adapter-behave/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='testit-adapter-behave',
-    version='2.1.7',
+    version='2.1.8',
     description='Behave adapter for Test IT',
     long_description=open('README.md', "r").read(),
     long_description_content_type="text/markdown",
@@ -22,6 +22,6 @@ setup(
     package_dir={'': 'src'},
     install_requires=[
         'behave',
-        'testit-python-commons==2.1.7',
+        'testit-python-commons==2.1.8',
         'attrs'],
 )
