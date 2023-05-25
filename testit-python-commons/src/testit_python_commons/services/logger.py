2bbe50cdae3c63a43d8bbe6705efe7c037f715e9
Pavel Butuzov <73579433+PavelButuzov@users.noreply.github.com>
2023-05-25 09:02:52 +0300

    Used "get_function_parameters" method from Utils in logger. (#77)
    Co-authored-by: Pavel Butuzov <pavel.butuzov@testit.software>

--- testit-python-commons/src/testit_python_commons/services/logger.py
+++ testit-python-commons/src/testit_python_commons/services/logger.py
@@ -7,9 +7,11 @@ from testit_python_commons.services.plugin_manager import TmsPluginManager
 def adapter_logger(function):
     @wraps(function)
     def wrapper(*args, **kwargs):
+        from testit_python_commons.services.utils import Utils
+
         logger = TmsPluginManager.get_logger()
 
-        parameters = get_function_parameters(function, *args, **kwargs)
+        parameters = Utils.get_function_parameters(function, *args, **kwargs)
 
         message = f'Method "{function.__name__}" started'
 
@@ -29,27 +31,3 @@ def adapter_logger(function):
 
         return result
     return wrapper
-
-
-def get_function_parameters(function, *args, **kwargs):
-    parameters = {}
-    args_default_values = inspect.getfullargspec(function).defaults
-
-    if args or args_default_values:
-        all_keys = inspect.getfullargspec(function).args
-        all_args = list(args)
-
-        if args_default_values:
-            all_args += list(args_default_values[len(args) - (len(all_keys) - len(args_default_values)):])
-
-        method_args = [arg_name for arg_name in all_keys if arg_name not in list(kwargs)]
-
-        if len(method_args) == len(all_args):
-            for index in range(0, len(method_args)):
-                parameters[method_args[index]] = str(all_args[index])
-
-    if kwargs:
-        for key, parameter in kwargs.items():
-            parameters[key] = str(parameter)
-
-    return parameters
