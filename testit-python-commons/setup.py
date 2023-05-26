a0f89dca1a7dbf10a2b1ccd5194335aaee72ec68
SupportAgent <128583967+Support-Agent@users.noreply.github.com>
2023-05-26 15:24:34 +0300

    Update setup.py

--- testit-python-commons/setup.py
+++ testit-python-commons/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='testit-python-commons',
-    version='2.1.9',
+    version='2.2.0',
     description='Python commons for Test IT',
     long_description=open('README.md', "r").read(),
     long_description_content_type="text/markdown",
