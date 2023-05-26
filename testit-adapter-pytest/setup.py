7cd0ee17092e6f7e3b81d5e6159358e88ec9611a
SupportAgent <128583967+Support-Agent@users.noreply.github.com>
2023-05-26 15:26:38 +0300

    Update setup.py

--- testit-adapter-pytest/setup.py
+++ testit-adapter-pytest/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='testit-adapter-pytest',
-    version='2.1.9',
+    version='2.2.0',
     description='Pytest adapter for Test IT',
     long_description=open('README.md', "r").read(),
     long_description_content_type="text/markdown",
