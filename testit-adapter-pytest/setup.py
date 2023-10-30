814dbc1fa5d3b3585efb1e503366992898125a94
Pavel Butuzov <73579433+PavelButuzov@users.noreply.github.com>
2023-10-30 09:25:25 +0300

    Updated version of packages. (#112)
    Co-authored-by: pavel.butuzov <pavel.butuzov@testit.software>

--- testit-adapter-pytest/setup.py
+++ testit-adapter-pytest/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='testit-adapter-pytest',
-    version='2.4.2',
+    version='2.4.3',
     description='Pytest adapter for Test IT',
     long_description=open('README.md', "r").read(),
     long_description_content_type="text/markdown",
@@ -20,6 +20,6 @@ setup(
     py_modules=['testit_adapter_pytest'],
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
-    install_requires=['pytest', 'pytest-xdist', 'attrs', 'testit-python-commons==2.4.2'],
+    install_requires=['pytest', 'pytest-xdist', 'attrs', 'testit-python-commons==2.4.3'],
     entry_points={'pytest11': ['testit_adapter_pytest = testit_adapter_pytest.plugin']}
 )
