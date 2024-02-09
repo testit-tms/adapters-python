7d8cb276feae6b38dc648533980708fb1ba8eb11
pavel.butuzov <pavel.butuzov@testit.software>
2024-02-09 10:35:49 +0300

    Supported "pytest.param".

--- testit-adapter-pytest/setup.py
+++ testit-adapter-pytest/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='testit-adapter-pytest',
-    version='2.5.3',
+    version='2.6.0',
     description='Pytest adapter for Test IT',
     long_description=open('README.md', "r").read(),
     long_description_content_type="text/markdown",
@@ -23,6 +23,6 @@ setup(
     py_modules=['testit_adapter_pytest'],
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
-    install_requires=['pytest', 'pytest-xdist', 'attrs', 'testit-python-commons==2.5.3'],
+    install_requires=['pytest', 'pytest-xdist', 'attrs', 'testit-python-commons==2.6.0'],
     entry_points={'pytest11': ['testit_adapter_pytest = testit_adapter_pytest.plugin']}
 )
