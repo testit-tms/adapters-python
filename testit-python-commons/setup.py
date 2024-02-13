0cb880277f6837bc1028e2363fba3262a5aed8ba
pavel.butuzov <pavel.butuzov@testit.software>
2024-02-13 11:31:13 +0300

    Refactored metadata collection from tests for Nose.

--- testit-python-commons/setup.py
+++ testit-python-commons/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='testit-python-commons',
-    version='2.6.0',
+    version='2.7.0',
     description='Python commons for Test IT',
     long_description=open('README.md', "r").read(),
     long_description_content_type="text/markdown",
