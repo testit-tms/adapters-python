c9e9b913c7a92f7fe7506c7dd142bf5f01cbfbc4
pavel.butuzov <pavel.butuzov@testit.software>
2025-03-26 15:11:04 +0300

    feat: update the api-client version.

--- testit-adapter-pytest/setup.py
+++ testit-adapter-pytest/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = "3.5.0"
+VERSION = "3.5.1"
 
 setup(
     name='testit-adapter-pytest',
@@ -23,6 +23,6 @@ setup(
     py_modules=['testit_adapter_pytest'],
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
-    install_requires=['pytest', 'pytest-xdist', 'attrs', 'testit-python-commons==3.5.0'],
+    install_requires=['pytest', 'pytest-xdist', 'attrs', 'testit-python-commons==3.5.1'],
     entry_points={'pytest11': ['testit_adapter_pytest = testit_adapter_pytest.plugin']}
 )
