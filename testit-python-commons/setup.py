f0986defe32f1d5cb3d181fcacac320c75c95467
pavel.butuzov <pavel.butuzov@testit.software>
2025-04-16 13:33:17 +0300

    feat: support api-client 6.1.

--- testit-python-commons/setup.py
+++ testit-python-commons/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = "3.6.1"
+VERSION = "3.7.0"
 
 setup(
     name='testit-python-commons',
@@ -25,5 +25,5 @@ setup(
     py_modules=['testit', 'testit_python_commons'],
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
-    install_requires=['pluggy', 'testit-api-client==6.0.1.post530']
+    install_requires=['pluggy', 'testit-api-client==6.1.0']
 )
