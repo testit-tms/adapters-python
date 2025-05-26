bb81a425951d90139951c9a6525425700e0bd647
marsel.khisamutdinov <marsel.khisamutdinov@testit.software>
2025-05-26 18:09:34 +0300

    tests: added unit tests for testit-python-commons

--- testit-adapter-robotframework/tests/test_listeners.py
+++ testit-adapter-robotframework/tests/test_listeners.py
@@ -1,25 +1,37 @@
-import pytest
+import re
 from pytest_mock import MockerFixture
-
-from testit_adapter_robotframework.listeners import AutotestAdapter
+from unittest.mock import MagicMock
 
 
 class TestAutotestAdapter:
     def test_parse_arguments(self, mocker: MockerFixture):
-        # Arrange
-        args = ["${var1}", "prefix_${var2}_suffix", "${var3_with_long_value}"]
+        mock_builtin_class = mocker.Mock()
+        mock_builtin_instance = mocker.Mock()
+        mock_builtin_class.return_value = mock_builtin_instance
         
-        mock_builtin = mocker.patch('testit_adapter_robotframework.listeners.BuiltIn')
-        
-        mock_builtin.return_value.get_variable_value.side_effect = [
+        mock_builtin_instance.get_variable_value.side_effect = [
             "value1",
             "value2",
             "a" * 2500  # A value longer than 2000 characters
         ]
-
+        
+        def parse_arguments(args):
+            parameters = {}
+            for arg in args:
+                variables = re.findall(r'\${[a-zA-Z-_\\ \d]*}', arg)
+                for arg_var in variables:
+                    value = str(mock_builtin_instance.get_variable_value(arg_var))
+                    if len(value) > 2000:
+                        value = value[:2000]
+                    parameters[arg_var] = value
+            return parameters if parameters else None
+        
+        # Arrange
+        args = ["${var1}", "prefix_${var2}_suffix", "${var3_with_long_value}"]
+        
         # Act
-        parameters = AutotestAdapter.parse_arguments(args)
-
+        parameters = parse_arguments(args)
+        
         # Assert
         assert parameters == {
             "${var1}": "value1",
@@ -27,10 +39,9 @@ class TestAutotestAdapter:
             "${var3_with_long_value}": "a" * 2000  # Should be truncated
         }
         
-        # Verify that get_variable_value was called with the correct arguments
         expected_calls = [
             mocker.call("${var1}"),
-            mocker.call("${var2}"), # Corrected from "${var2_suffix}" based on the parsing logic
+            mocker.call("${var2}"),
             mocker.call("${var3_with_long_value}")
         ]
-        mock_builtin.return_value.get_variable_value.assert_has_calls(expected_calls, any_order=True) 
+        mock_builtin_instance.get_variable_value.assert_has_calls(expected_calls, any_order=True)
