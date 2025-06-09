8b412da258de16764df0fe5558648cd4d7abbb55
pavel.butuzov <pavel.butuzov@testit.software>
2025-06-09 10:31:56 +0300

    feat: support api-client 6.1.

--- testit-adapter-nose/setup.py
+++ testit-adapter-nose/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "3.7.0"
+VERSION = "3.6.2"
 
 setup(
     name='testit-adapter-nose',
--- testit-python-commons/setup.py
+++ testit-python-commons/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = "3.7.0"
+VERSION = "3.6.2"
 
 setup(
     name='testit-python-commons',
