16dc2b946574b415e07d3a222182444e845491dc
pavel.butuzov <pavel.butuzov@testit.software>
2025-07-09 19:57:24 +0300

    feat: update the package versions.

--- testit-adapter-nose/setup.py
+++ testit-adapter-nose/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "3.6.4"
+VERSION = "3.6.5"
 
 setup(
     name='testit-adapter-nose',
--- testit-python-commons/setup.py
+++ testit-python-commons/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = "3.6.4"
+VERSION = "3.6.5"
 
 setup(
     name='testit-python-commons',
