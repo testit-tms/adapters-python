4c28e861f96208a793c7b99d40af0969ea9ad6a2
pavel.butuzov <pavel.butuzov@testit.software>
2025-07-17 19:09:00 +0300

    fix: fix the api-client with the CustomAttributeSearchResponseModel model.

--- testit-python-commons/setup.py
+++ testit-python-commons/setup.py
@@ -25,5 +25,5 @@ setup(
     py_modules=['testit', 'testit_python_commons'],
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
-    install_requires=['pluggy', 'tomli', 'testit-api-client==6.3.0.post540']
+    install_requires=['pluggy', 'tomli', 'testit-api-client==6.3.1.post540']
 )
