b40c646a5ecd2fb0220401b64f4f887447151222
pavel.butuzov <pavel.butuzov@testit.software>
2025-07-17 19:19:05 +0300

    feat: TMS-34098: support tms cloud.

--- testit-adapter-behave/setup.py
+++ testit-adapter-behave/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = "3.6.6.post540"
+VERSION = "3.6.6"
 
 setup(
     name='testit-adapter-behave',
--- testit-python-commons/setup.py
+++ testit-python-commons/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = "3.6.6.post540"
+VERSION = "3.6.6"
 
 setup(
     name='testit-python-commons',
@@ -25,5 +25,5 @@ setup(
     py_modules=['testit', 'testit_python_commons'],
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
-    install_requires=['pluggy', 'tomli', 'testit-api-client==6.3.1.post540']
+    install_requires=['pluggy', 'tomli', 'testit-api-client==6.3.0']
 )
