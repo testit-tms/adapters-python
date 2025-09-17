a2369f0dc91ba841a0f54b6f58df017d89fc4353
pavel.butuzov <pavel.butuzov@testit.software>
2025-09-17 17:41:35 +0300

    fix: fix creating autotests by threads with the "importRealtime = false" parameter.

--- testit-python-commons/src/testit_python_commons/client/models/threads_for_update_and_result.py
+++ testit-python-commons/src/testit_python_commons/client/models/threads_for_update_and_result.py
@@ -7,7 +7,7 @@ class ThreadForUpdateAndResult:
     def __init__(
             self,
             thread_for_update: Dict[str, AutoTestPutModel],
-            thread_results_for_updated_autotests: List[AutoTestResultsForTestRunModel],
+            thread_results_for_updated_autotests: Dict[str, AutoTestResultsForTestRunModel],
             thread_for_autotest_links_to_wi_for_update: Dict[str, List[str]]
     ):
         self.__thread_for_update = thread_for_update
@@ -17,7 +17,7 @@ class ThreadForUpdateAndResult:
     def get_thread_for_update(self) -> Dict[str, AutoTestPutModel]:
         return self.__thread_for_update
 
-    def get_thread_results_for_updated_autotests(self) -> List[AutoTestResultsForTestRunModel]:
+    def get_thread_results_for_updated_autotests(self) -> Dict[str, AutoTestResultsForTestRunModel]:
         return self.__thread_results_for_updated_autotests
 
     def get_thread_for_autotest_links_to_wi_for_update(self) -> Dict[str, List[str]]:
@@ -27,19 +27,19 @@ class ThreadForUpdateAndResult:
 class ThreadsForUpdateAndResult:
     def __init__(
             self,
-            threads_for_update: List[Dict[str, AutoTestPutModel]],
-            threads_results_for_updated_autotests: List[List[AutoTestResultsForTestRunModel]],
-            threads_for_autotest_links_to_wi_for_update: List[Dict[str, List[str]]]
+            threads_for_update: Dict[str, AutoTestPutModel],
+            threads_results_for_updated_autotests: List[Dict[str, AutoTestResultsForTestRunModel]],
+            threads_for_autotest_links_to_wi_for_update: Dict[str, List[str]]
     ):
         self.__threads_for_update = threads_for_update
         self.__threads_results_for_updated_autotests = threads_results_for_updated_autotests
         self.__threads_for_autotest_links_to_wi_for_update = threads_for_autotest_links_to_wi_for_update
 
-    def get_threads_for_update(self) -> List[Dict[str, AutoTestPutModel]]:
+    def get_threads_for_update(self) -> Dict[str, AutoTestPutModel]:
         return self.__threads_for_update
 
-    def get_threads_results_for_updated_autotests(self) -> List[List[AutoTestResultsForTestRunModel]]:
+    def get_threads_results_for_updated_autotests(self) -> List[Dict[str, AutoTestResultsForTestRunModel]]:
         return self.__threads_results_for_updated_autotests
 
-    def get_threads_for_autotest_links_to_wi_for_update(self) -> List[Dict[str, List[str]]]:
+    def get_threads_for_autotest_links_to_wi_for_update(self) -> Dict[str, List[str]]:
         return self.__threads_for_autotest_links_to_wi_for_update
