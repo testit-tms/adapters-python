7240abdde855356caea49ea48711c71c357133cb
Dmitry Ermakovich <dmitry.ermakovich@testit.software>
2026-03-04 19:34:54 +0300

    chore: update versions

--- update_versions.sh
+++ update_versions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NEW_VERSION="4.0.0"
-TESTIT_API_CLIENT_VERSION="7.5.2"
+TESTIT_API_CLIENT_VERSION="7.5.3"
 
 echo "Updating all adapters to version: $NEW_VERSION"
 echo "Updating testit-api-client to version: $TESTIT_API_CLIENT_VERSION"
