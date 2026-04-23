41f8e53111de731e76ef9f22155609840cd4fcdf
Dmitry Ermakovich <dmitry.ermakovich@testit.software>
2026-04-23 18:44:23 +0300

    chore: versions

--- testit-adapter-behave/setup.py
+++ testit-adapter-behave/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = "4.1.10"
+VERSION = "4.1.10.post570"
 
 setup(
     name='testit-adapter-behave',
--- update_versions.sh
+++ update_versions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-NEW_VERSION="4.1.10"
-TESTIT_API_CLIENT_VERSION="7.5.5"
+NEW_VERSION="4.1.10.post570"
+TESTIT_API_CLIENT_VERSION="7.5.5.post570"
 
 echo "Updating all adapters to version: $NEW_VERSION"
 echo "Updating testit-api-client to version: $TESTIT_API_CLIENT_VERSION"
