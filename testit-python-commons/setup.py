2e265beea9f7544f78a3c81bb38aeb54b6a9c26b
Dmitry Ermakovich <dmitry.ermakovich@testit.software>
2026-06-10 23:05:29 +0300

    chore: bump v

--- testit-python-commons/setup.py
+++ testit-python-commons/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = "4.2.5"
+VERSION = "4.2.6"
 
 setup(
     name='testit-python-commons',
--- update_versions.sh
+++ update_versions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NEW_VERSION="4.2.5"
+NEW_VERSION="4.2.6"
 TESTIT_API_CLIENT_VERSION="7.5.10"
 
 echo "Updating all adapters to version: $NEW_VERSION"
