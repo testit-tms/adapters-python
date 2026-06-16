8deb4bffc523f07b26dd293596f1e40aa3b14571
Dmitry Ermakovich <dmitry.ermakovich@testit.software>
2026-06-16 10:20:46 +0300

    chore: version

--- testit-adapter-nose/setup.py
+++ testit-adapter-nose/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "4.2.7.post570"
+VERSION = "4.2.8.post570"
 
 setup(
     name='testit-adapter-nose',
--- testit-python-commons/setup.py
+++ testit-python-commons/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = "4.2.7.post570"
+VERSION = "4.2.8.post570"
 
 setup(
     name='testit-python-commons',
