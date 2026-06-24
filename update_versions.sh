16b1a81f048f0bdc5556c4670a1873c2eb01d5bb
Dmitry Ermakovich <dmitry.ermakovich@testit.software>
2026-06-24 12:38:35 +0300

    feat: support cloud (#288)

--- update_versions.sh
+++ update_versions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-NEW_VERSION="4.2.8"
-TESTIT_API_CLIENT_VERSION="7.5.10"
+NEW_VERSION="4.2.9"
+TESTIT_API_CLIENT_VERSION="7.5.11"
 
 echo "Updating all adapters to version: $NEW_VERSION"
 echo "Updating testit-api-client to version: $TESTIT_API_CLIENT_VERSION"
